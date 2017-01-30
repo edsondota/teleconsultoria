803f6652ae9f49a6ec36b69c66230e123fbaac5d
Edson Dota <edson.dota@sisqualis.com.br>
2017-01-30 20:26:15 -0200

    Adiciona teste para criar o solicitante

--- teleconsultoria/test.py
+++ teleconsultoria/test.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
-from teleconsultoria.models import Administrador
 from django.contrib.auth.models import User
+from teleconsultoria.models import Administrador, Solicitante
 
 
 class AdministradorTest(TestCase):
@@ -9,3 +9,11 @@ class AdministradorTest(TestCase):
         user_administrador = User.objects.create(username="administrador", password="123", email="admin@email.com", is_superuser=True)
         administrador = Administrador.objects.create(user=user_administrador)
         self.assertTrue(administrador)
+
+
+class SolicitanteTest(TestCase):
+    def test_cria_solicitante(self):
+        """ Testando se é possível criar um objeto Solicitante """
+        user_solicitante = User.objects.create(username="solicitante", password="123", email="solicitante@email.com")
+        solicitante = Solicitante.objects.create(user=user_solicitante, nome=u"João Silva", telefone="551122336655", cpf="11122233344")
+        self.assertTrue(solicitante)
