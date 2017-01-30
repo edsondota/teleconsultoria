c0e8c6d1f2fafe90276f01a1ff01dab528b110d1
Edson Dota <edson.dota@sisqualis.com.br>
2017-01-30 20:56:11 -0200

    Adiciona teste para criar Teleconsultoria

--- teleconsultoria/test.py
+++ teleconsultoria/test.py
@@ -3,6 +3,7 @@ import datetime
 from django.test import TestCase
 from django.contrib.auth.models import User
 from teleconsultoria.models import Administrador, Solicitante, Teleconsultor
+from teleconsultoria.models import Teleconsultoria
 
 
 class AdministradorTest(TestCase):
@@ -32,3 +33,22 @@ class TeleconsultorTest(TestCase):
         teleconsultor = Teleconsultor.objects.create(user=user_teleconsultor,
                 nome="José Santos", crm="BA-1234", data_formatura=datetime.date(2017, 1, 30))
         self.assertTrue(teleconsultor)
+
+
+class TeleconsultoriaTest(TestCase):
+    def setUp(self):
+        user_teleconsultor = User.objects.create(username="teleconsultor",
+                password="123", email="teleconsultor@email.com")
+        Teleconsultor.objects.create(user=user_teleconsultor,
+                nome="José Santos", crm="BA-1234", data_formatura=datetime.date(2017, 1, 30))
+        user_solicitante = User.objects.create(username="solicitante",
+                password="123", email="solicitante@email.com")
+        Solicitante.objects.create(user=user_solicitante,
+                nome=u"João Silva", telefone="551122336655", cpf="11122233344")
+    def test_cria_teleconsultoria(self):
+        """ Testando se é possível criar um objeto Teleconsultoria """
+        teleconsultor = Teleconsultor.objects.get(crm='BA-1234')
+        solicitante = Solicitante.objects.get(cpf="11122233344")
+        teleconsultoria = Teleconsultoria.objects.create(teleconsultor=teleconsultor,
+                solicitante=solicitante, agendamento_teleconsultoria=datetime.datetime(2017, 1, 30, 20, 52))
+        self.assertTrue(teleconsultoria)
