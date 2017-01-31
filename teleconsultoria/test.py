1c118d106d286dbd286684c68feb240922584ccb
Edson Dota <edson.dota@sisqualis.com.br>
2017-01-30 22:05:13 -0200

    Refatora testes e adiciona setUp como melhoria

--- teleconsultoria/test.py
+++ teleconsultoria/test.py
@@ -16,22 +16,31 @@ class AdministradorTest(TestCase):
 
 
 class SolicitanteTest(TestCase):
-    def test_cria_solicitante(self):
-        """ Testando se é possível criar um objeto Solicitante """
+    def setUp(self):
         user_solicitante = User.objects.create(username="solicitante",
                 password="123", email="solicitante@email.com")
         solicitante = Solicitante.objects.create(user=user_solicitante,
                 nome=u"João Silva", telefone="551122336655", cpf="11122233344")
+
+    def test_cria_solicitante(self):
+        """ Testando se é possível criar um objeto Solicitante """
+        solicitante = Solicitante.objects.get(cpf="11122233344")
         self.assertTrue(solicitante)
+
+    def test_solicitante_unico(self):
+        pass
 
 
 class TeleconsultorTest(TestCase):
-    def test_cria_teleconsultor(self):
-        """ Testando se é possível criar um objeto Teleconsultor """
+    def setUp(self):
         user_teleconsultor = User.objects.create(username="teleconsultor",
                 password="123", email="teleconsultor@email.com")
         teleconsultor = Teleconsultor.objects.create(user=user_teleconsultor,
                 nome="José Santos", crm="BA-1234", data_formatura=datetime.date(2017, 1, 30))
+
+    def test_cria_teleconsultor(self):
+        """ Testando se é possível criar um objeto Teleconsultor """
+        teleconsultor = Teleconsultor.objects.get(crm='BA-1234')
         self.assertTrue(teleconsultor)
 
 
