19c12360e4923be75ed4b9ec2286b3a4497df52f
Edson Dota <edson.dota@sisqualis.com.br>
2017-02-03 12:01:50 -0200

    Adiciona view para login

--- teleconsultoria/urls.py
+++ teleconsultoria/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from teleconsultoria.views import LoginView
+from django.contrib.auth.decorators import login_required
+from teleconsultoria.views import LoginView, PainelAdminView
 
 urlpatterns = [
-
     url(r'^$', LoginView.as_view(), name="login_view"),
+    url(r'^painel/administracao$', 
+        login_required(PainelAdminView.as_view()), name="painel_view"),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- teleconsultoria/views.py
+++ teleconsultoria/views.py
@@ -1,5 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.views.generic import View
@@ -8,3 +9,18 @@ from django.views.generic import View
 class LoginView(View):
     def get(self, request):
         return render(request, 'teleconsultoria/login.html', locals())
+
+    def post(self, request):
+        if request.POST.get('username') and request.POST.get('password'):
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None and user.is_active:
+                login(request, user)
+                return redirect('painel_view')
+        return redirect('login_view')
+
+
+class PainelAdminView(View):
+    def get(self, request):
+        return render(request, 'teleconsultoria/painel_admin.html', locals())
