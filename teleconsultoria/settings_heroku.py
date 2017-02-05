973f41cec0f9176f4e0cff4af561ca8aa6dd4f16
Edson Dota <edson.dota@sisqualis.com.br>
2017-02-05 21:34:55 -0200

    Corrige host do psql do heroku

--- teleconsultoria/settings_heroku.py
+++ teleconsultoria/settings_heroku.py
@@ -6,11 +6,11 @@ DEBUG = False
 
 DATABASES = {
     'default': {
-        'ENGINE': 'ec2-23-21-111-81.compute-1.amazonaws.com',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'dcovfsd05b6gfg',
         'USER': 'blguriuehevnth',
         'PASSWORD': 'b1ff315293a2e29222d3d11104ca61cc81f76911b6bc374bf2411237eb043a13',
-        'HOST': 'localhost',
+        'HOST': 'ec2-23-21-111-81.compute-1.amazonaws.com',
         'PORT': '5432',
     }
 }
