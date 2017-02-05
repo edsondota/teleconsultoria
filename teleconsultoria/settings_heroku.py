758b695cb4bef2e4d23934d1a3ec12aec55fb4d5
Edson Dota <edson.dota@sisqualis.com.br>
2017-02-05 21:45:31 -0200

    Adiciona configurações de banco para heroku

--- teleconsultoria/settings_heroku.py
+++ teleconsultoria/settings_heroku.py
@@ -1,16 +1,10 @@
 from teleconsultoria.settings import *
+import dj_database_url
 
 ALLOWED_HOSTS = ['teleconsultoria.herokuapp.com',]
 
 DEBUG = False
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dcovfsd05b6gfg',
-        'USER': 'blguriuehevnth',
-        'PASSWORD': 'b1ff315293a2e29222d3d11104ca61cc81f76911b6bc374bf2411237eb043a13',
-        'HOST': 'ec2-23-21-111-81.compute-1.amazonaws.com',
-        'PORT': '5432',
-    }
-}
+db_from_env = dj_database_url.config()
+
+DATABASES['default'].update(db_from_env)
